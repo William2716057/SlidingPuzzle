c7bcb10e02ba280293c5dfa0d17622a2951ba8d4
User <Willspri@gmail.com>
2024-01-20 14:16:04 +1300

    tiles

--- SlidingPuzzle/main.js
+++ SlidingPuzzle/main.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+
+let size = 4
+//let size = 4;
+let numberOfTiles = size ** 2;
+let highlighted = numberOfTiles;
+let shuffled = false;
+
+let buttonContainer = document.getElementById('tiles');
+
