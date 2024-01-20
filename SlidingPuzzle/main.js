fd1c7463f451060aa6edbe7eda5a8f856ecc4c16
User <Willspri@gmail.com>
2024-01-20 14:29:00 +1300

    added

--- SlidingPuzzle/main.js
+++ SlidingPuzzle/main.js
@@ -3,7 +3,13 @@
 let size = 4
 //calculate the number of tiles
 let numberOfTiles = size ** 2;
+//for highlighting tiles when clicked or tapped
 let highlighted = numberOfTiles;
+//for shuffling the tiles
 let shuffled = false;
-
+//reference container for puzzle buttons
 let buttonContainer = document.getElementById('tiles');
+
+//initialise new game
+newGame();
+
